c98fbd7e3f631942b60d4459cc90e84409b10f95
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-09-01 14:31:52 -0300

    config new ENV

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -11,10 +11,11 @@ module.exports = {
   },
   test: {
     username: "root",
-    password: null,
-    database: "database_test",
+    password: "root",
+    database: "vivebio_db",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
+    port: 3306
   },
   production: {
     username: "root",
